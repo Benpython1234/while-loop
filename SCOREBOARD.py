08e9f71657cce7d3c5554e8d1b735d52bc96e524
Benpython1234 <benjoash15@gmail.com>
2025-02-24 18:18:28 +1100

    Add files via upload

---
+++ SCOREBOARD.py
@@ -0,0 +1,15 @@
+'''
+target_score=150
+
+current_score=0
+
+while current_score <target_score:
+    runs=int(input("Enter runs scores in this ball: "))
+    current_score=current_score+runs
+    print(f"Current score: {current_score} / {target_score}")
+print("Target Reached! you won the match!!!!")
+
+'''
+i=0
+while i<=0:
+    print("I WILL RUN FOREVER")
